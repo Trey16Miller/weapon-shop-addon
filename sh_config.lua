9ed30f9459283d36b73eae46678cadfb4b40561e
Craigy131 <65000316+Trey16Miller@users.noreply.github.com>
2026-01-19 03:05:18 -0500

    Create sh_config.lua

---
+++ sh_config.lua
@@ -0,0 +1,13 @@
+NPCSHOP = NPCSHOP or {}
+
+NPCSHOP.KillReward = 25
+
+NPCSHOP.Weapons = {
+    { name = "Pistol",   class = "weapon_pistol",  cost = 100 },
+    { name = "SMG",      class = "weapon_smg1",    cost = 250 },
+    { name = "Shotgun",  class = "weapon_shotgun", cost = 400 },
+    { name = "AR2",      class = "weapon_ar2",     cost = 700 },
+    { name = ".357",     class = "weapon_357",     cost = 500 },
+    { name = "Crossbow", class = "weapon_crossbow",cost = 600 },
+    { name = "RPG",      class = "weapon_rpg",     cost = 1200 },
+}
